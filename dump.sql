5d6e1cc6f7e1a64c60c95fb79d69be6b7b32c639
Tiffany Benjamin <tiffany.t.benjamin@gmail.com>
2016-06-22 20:10:26 +0000

    adding database dump

--- dump.sql
+++ dump.sql
@@ -1,8 +1,9 @@
 -- MySQL dump 10.13  Distrib 5.7.12, for Linux (x86_64)
 --
--- Host: localhost    Database: globitek
+-- Host: localhost    Database: assignment01
 -- ------------------------------------------------------
 -- Server version	5.7.12-0ubuntu1
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -12,7 +13,9 @@
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;--
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
 -- Table structure for table `salespeople`
 --
 
@@ -65,6 +68,30 @@ INSERT INTO `salespeople_territories` VALUES (1,1,1),(2,2,4),(3,3,3),(4,4,6),(5,
 UNLOCK TABLES;
 
 --
+-- Table structure for table `states`
+--
+
+DROP TABLE IF EXISTS `states`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `states` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(15) CHARACTER SET utf8 DEFAULT NULL,
+  `code` varchar(3) CHARACTER SET utf8 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `states`
+--
+
+LOCK TABLES `states` WRITE;
+/*!40000 ALTER TABLE `states` DISABLE KEYS */;
+INSERT INTO `states` VALUES (1,'Alabama ','AL'),(2,'Alaska ','AK'),(3,'Arizona ','AZ'),(4,'Arkansas ','AR'),(5,'California ','CA'),(6,'Colorado ','CO'),(7,'Connecticut ','CT'),(8,'Delaware ','DE'),(9,'Florida ','FL'),(10,'Georgia ','GA'),(11,'Hawaii ','HI'),(12,'Idaho ','ID'),(13,'Illinois ','IL'),(14,'Indiana ','IN'),(15,'Iowa ','IA'),(16,'Kansas ','KS'),(17,'Kentucky ','KY'),(18,'Louisiana ','LA'),(19,'Maine ','ME'),(20,'Maryland ','MD'),(21,'Massachusetts ','MA'),(22,'Michigan ','MI'),(23,'Minnesota ','MN'),(24,'Mississippi ','MS'),(25,'Missouri ','MO'),(26,'Montana ','MT'),(27,'Nebraska ','NE'),(28,'Nevada ','NV'),(29,'New Hampshire ','NH'),(30,'New Jersey ','NJ'),(31,'New Mexico ','NM'),(32,'New York ','NY'),(33,'North Carolina ','NC'),(34,'North Dakota ','ND'),(35,'Ohio ','OH'),(36,'Oklahoma ','OK'),(37,'Oregon ','OR'),(38,'Pennsylvania ','PA'),(39,'Rhode Island ','RI'),(40,'South Carolina ','SC'),(41,'South Dakota ','SD'),(42,'Tennessee ','TN'),(43,'Texas ','TX'),(44,'Utah ','UT'),(45,'Vermont ','VT'),(46,'Virginia ','VA'),(47,'Washington ','WA'),(48,'West Virginia ','WV'),(49,'Wisconsin ','WI'),(50,'Wyoming ','WY');
+/*!40000 ALTER TABLE `states` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `territories`
 --
 
@@ -99,4 +126,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-10 21:44:12
+-- Dump completed on 2016-06-22 20:06:51
